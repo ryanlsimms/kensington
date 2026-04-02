29274e9ca5ae8d5ba00a5ea58449bb794f574198
Ryan Lee Simms <ryan_simms@apple.com>
2026-04-02 10:27:32 -0400

    refactor browser tests

--- tests/browser/server/html-server.js
+++ tests/browser/server/html-server.js
@@ -5,19 +5,21 @@ import { fileURLToPath } from 'node:url';
 import buildModule from './build-module.js';
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
-const html = fs.readFileSync(path.join(dirname, 'browser-test.html'));
 const kensington = await buildModule(path.join(dirname, '..', '..', '..', 'esm', 'kensington.js'));
 
 http.createServer(async (req, res) =>  {
   res.status = 200
-  if (req.url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    return res.end(html);
-  } else if (req.url === '/kensington.js') {
+  if (req.url === '/kensington.js') {
     res.setHeader('Content-Type', 'text/javascript');
     return res.end(kensington);
   } else {
-    res.status = 404;
-    res.end('');
+    try {
+      const html = fs.readFileSync(path.join(dirname, '..', 'pages', req.url));
+      res.setHeader('Content-Type', 'text/html');
+      return res.end(html);
+    } catch {
+      res.status = 404;
+      return res.end('');
+    }
   }
 }).listen(9615);
